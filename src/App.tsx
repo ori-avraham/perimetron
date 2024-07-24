80f7332e6eafb8935a2bf9201a741d13361e8c0d
Ori Avraham <mr.ori.avraham@gmail.com>
2024-07-24 18:42:30 +0700

    heading styles and color

--- src/App.tsx
+++ src/App.tsx
@@ -12,13 +12,15 @@ function App() {
         <PlaybackProvider>
           <Header />
           <div className="text-center mt-6">
-            <h1 className="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl">
-              <span className="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">
+            <h1 className="mb-4 text-3xl font-extrabold text-primary md:text-5xl lg:text-6xl">
+              <span
+                className="text-transparent bg-clip-text bg-gradient-to-r to-[#9d7cd8] dark:to-[#bb9af7] from-[#ff9e64] dark:from-[#ffc777]"
+              >
                 Perimetron
               </span>{" "}
               Simulator
             </h1>
-            <p className="text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400">
+            <p className="text-lg font-normal lg:text-xl text-muted-foreground">
               Infinite PI
             </p>
           </div>
